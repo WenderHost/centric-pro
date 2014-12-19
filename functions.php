58096b6799c040248c033a7f48587c409d98ac55
Michael Wender <michael@michaelwender.com>
2014-12-19 16:23:41 -0500

    Updating .site-footer with proper text and increasing font size

--- functions.php
+++ functions.php
@@ -37,6 +37,13 @@ function centric_load_scripts() {
 
 }
 
+//* Footer Copyright
+add_filter('genesis_footer_creds_text', 'caseanti_footer_creds_filter');
+function caseanti_footer_creds_filter( $creds ) {
+	$creds = '[footer_copyright] ' . get_bloginfo( 'title' ) . '. All rights reserved.<br />Auctions in Association with:<br />Case Antiques, Inc. - TNGL#5157, TNGBL#5387 - 2240 Sutherland Avenue, Suite 101 - Knoxville, TN 37919 - (865) 558-3033';
+	return $creds;
+}
+
 //* Add new image sizes
 add_image_size( 'featured-page', 960, 700, TRUE );
 add_image_size( 'featured-post', 400, 300, TRUE );
