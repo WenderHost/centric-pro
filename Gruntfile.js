c4e54ff2e55dc6c00ecea2389eb7e6fdb0692321
Michael Wender <michael@michaelwender.com>
2015-08-25 15:30:00 -0400

    Adding build tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,17 @@ module.exports = function(grunt) {
     less: {
       development: {
         options: {
+          compress: false,
+          yuicompress: false,
+          optimization: 2
+        },
+        files: {
+          // target.css file: source.less file
+          'lib/css/main.css': 'lib/less/main.less'
+        }
+      },
+      production: {
+        options: {
           compress: true,
           yuicompress: true,
           optimization: 2
@@ -11,11 +22,11 @@ module.exports = function(grunt) {
           // target.css file: source.less file
           'lib/css/main.css': 'lib/less/main.less'
         }
-      }
+      },
     },
     watch: {
       options: {
-        livereload: 35712,
+        livereload: true,
       },
       styles: {
         files: ['lib/less/**/*.less'], // which files to watch
@@ -29,6 +40,7 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-watch');
-
+  grunt.registerTask('builddev', ['less:development']);
+  grunt.registerTask('build', ['less:production']);
   grunt.registerTask('default', ['watch']);
 };
