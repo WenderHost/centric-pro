3c9b713907be077253660820774fa54371fcda1e
Michael Wender <michael@michaelwender.com>
2015-10-21 12:17:07 -0400

    Removing extra lines

--- js/global.js
+++ js/global.js
@@ -9,17 +9,16 @@ jQuery(function( $ ){
 	    $('.site-header').removeClass('shrink');
 	  }
 	});
-    
+
     $("header .genesis-nav-menu").addClass("responsive-menu").before('<div id="responsive-menu-icon"></div>');
-    
+
     $("#responsive-menu-icon").click(function(){
     	$("header .genesis-nav-menu").slideToggle();
     });
-    
+
     $(window).resize(function(){
     	if(window.innerWidth > 600) {
     		$("header .genesis-nav-menu").removeAttr("style");
     	}
     });
-	
 });
