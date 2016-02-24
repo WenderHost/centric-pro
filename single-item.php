a5d32c9bbfeeb148c841d9088a88f9d858a8d396
Michael Wender <michael@michaelwender.com>
2016-02-24 11:38:12 -0500

    Running Tidy PHP

--- single-item.php
+++ single-item.php
@@ -21,7 +21,7 @@ add_action( 'genesis_entry_content', 'centric_item_attachments', 20 ); // Add at
  *
  * @return void
  */
-function centric_item_attachments(){
+function centric_item_attachments() {
 	global $post;
 
 	$parent_title = get_the_title( $post->ID );
@@ -35,9 +35,9 @@ function centric_item_attachments(){
 	);
 	$images = get_children( $args );
 
-	if( $images ){
+	if ( $images ) {
 		$gallery_images = array();
-		foreach( $images as $attachment_id => $attachment ){
+		foreach ( $images as $attachment_id => $attachment ) {
 			$image_ids[] = $attachment_id;
 			$fullsize = wp_get_attachment_image_src( $attachment_id, 'fullsize' );
 			$thumbnail = wp_get_attachment_image_src( $attachment_id, 'thumbnail' );
@@ -55,71 +55,71 @@ function centric_item_attachments(){
 /**
  * Adds .moreinfo box to item content.
  *
- * @see get_metadata() 		used to retrieve custom meta data added to an item's auction
+ * @see get_metadata()   used to retrieve custom meta data added to an item's auction
  * @global object $post WordPress post object.
  *
  * @since 1.0.0
  *
  * @return void
  */
-function centric_item_auctioninfo(){
+function centric_item_auctioninfo() {
 	global $post;
 	$terms = wp_get_object_terms( $post->ID, 'auction' );
-	?>
+?>
     <div class="moreinfo alignright">
     	<ul>
 		<?php
-		$high_est = get_post_meta( $post->ID, '_high_est', true );
-		$low_est = get_post_meta( $post->ID, '_low_est', true );
-		$realized = get_post_meta( $post->ID, '_realized', true );
-		$lotnum = get_post_meta( $post->ID, '_lotnum', true );
+	$high_est = get_post_meta( $post->ID, '_high_est', true );
+	$low_est = get_post_meta( $post->ID, '_low_est', true );
+	$realized = get_post_meta( $post->ID, '_realized', true );
+	$lotnum = get_post_meta( $post->ID, '_lotnum', true );
 
-		if( $terms ){
-			foreach( $terms as $term ){
-				if( $term->taxonomy == 'auction' ){
-					$meta = get_metadata( 'auction', $term->term_id, 'meta', true );
-					$button_classes = array( 'button' );
+	if ( $terms ) {
+		foreach ( $terms as $term ) {
+			if ( $term->taxonomy == 'auction' ) {
+				$meta = get_metadata( 'auction', $term->term_id, 'meta', true );
+				$button_classes = array( 'button' );
 
-					$auction_timestamp = strtotime( get_metadata( 'auction', $term->term_id, 'date', true ) );
-					$current_timestamp = strtotime( date( 'Y-m-d', current_time( 'timestamp' ) ) );
-					if( $current_timestamp < $auction_timestamp ){
-						$link_text = 'Bid Now';
-						$button_classes[] = 'green';
-					} else {
-						if( ! $realized )
-							$realized = 'PASSED';
-						$link_text = 'View Final Price';
-					}
+				$auction_timestamp = strtotime( get_metadata( 'auction', $term->term_id, 'date', true ) );
+				$current_timestamp = strtotime( date( 'Y-m-d', current_time( 'timestamp' ) ) );
+				if ( $current_timestamp < $auction_timestamp ) {
+					$link_text = 'Bid Now';
+					$button_classes[] = 'green';
+				} else {
+					if ( ! $realized )
+						$realized = 'PASSED';
+					$link_text = 'View Final Price';
+				}
 
-					if( ! empty( $realized ) && is_numeric( $realized ) ){
-						echo '<li><h1 style="text-align: center;">SOLD! <span style="font-weight: normal">for ' . AuctionShortcodes::format_price( $realized ) . '.</span></h1></li>';
-					}
-					/*
+				if ( ! empty( $realized ) && is_numeric( $realized ) ) {
+					echo '<li><h1 style="text-align: center;">SOLD! <span style="font-weight: normal">for ' . AuctionShortcodes::format_price( $realized ) . '.</span></h1></li>';
+				}
+				/*
 					else if( 'PASSED' == $realized ){
 						echo '<li><h1 style="text-align: center;">PASSED</h1></li>';
 					}
 					/**/
 
-					if( ! empty( $meta['auction_id'] ) ){
-						if( ! $lotnum )
-							$lotnum = get_post_meta( $post->ID, '_lotnum', true );
+				if ( ! empty( $meta['auction_id'] ) ) {
+					if ( ! $lotnum )
+						$lotnum = get_post_meta( $post->ID, '_lotnum', true );
 
-						if( 'PASSED' != $realized )
-							echo '<li><a class="' . implode( ' ', $button_classes ) . '" target="_blank" href="http://www.liveauctioneers.com/itemLookup/'.$meta['auction_id'].'/'.$lotnum.'">'.$link_text.'</a></li>';
-					}
-					if( null != get_post_meta( $post->ID, '_igavel_lotnum', true ) ){
-						$igavel_lotnum = get_post_meta( $post->ID, '_igavel_lotnum', true );
-						$igavel_item_url = 'http://bid.igavelauctions.com/Bidding.taf?_function=detail&Auction_uid1=' . $igavel_lotnum;
-						echo '<li><a class="' . implode( ' ', $button_classes ) . '" target="_blank" href="' . $igavel_item_url . '" title="View ' . esc_attr( get_the_title() ) . ' on iGavel">' . $link_text . '</a></li>';
-					}
+					if ( 'PASSED' != $realized )
+						echo '<li><a class="' . implode( ' ', $button_classes ) . '" target="_blank" href="http://www.liveauctioneers.com/itemLookup/'.$meta['auction_id'].'/'.$lotnum.'">'.$link_text.'</a></li>';
+				}
+				if ( null != get_post_meta( $post->ID, '_igavel_lotnum', true ) ) {
+					$igavel_lotnum = get_post_meta( $post->ID, '_igavel_lotnum', true );
+					$igavel_item_url = 'http://bid.igavelauctions.com/Bidding.taf?_function=detail&Auction_uid1=' . $igavel_lotnum;
+					echo '<li><a class="' . implode( ' ', $button_classes ) . '" target="_blank" href="' . $igavel_item_url . '" title="View ' . esc_attr( get_the_title() ) . ' on iGavel">' . $link_text . '</a></li>';
 				}
 			}
 		}
+	}
 
-		if(!empty($low_est)) echo '<li><strong>Low Estimate:</strong> '.AuctionShortcodes::format_price( $low_est ). '</li>';
-		if(!empty($high_est)) echo '<li><strong>High Estimate:</strong> '.AuctionShortcodes::format_price( $high_est ). '</li>';
-		if( ! empty( $realized ) && is_numeric( $realized ) ) echo '<li itemprop="offers" itemscope="itemscope" itemtype="http://schema.org/Offer"><strong>Realized:</strong> <span itemprop="price">'.AuctionShortcodes::format_price( $realized ). '</span></li>';
-		?>
+	if ( !empty( $low_est ) ) echo '<li><strong>Low Estimate:</strong> '.AuctionShortcodes::format_price( $low_est ). '</li>';
+	if ( !empty( $high_est ) ) echo '<li><strong>High Estimate:</strong> '.AuctionShortcodes::format_price( $high_est ). '</li>';
+	if ( ! empty( $realized ) && is_numeric( $realized ) ) echo '<li itemprop="offers" itemscope="itemscope" itemtype="http://schema.org/Offer"><strong>Realized:</strong> <span itemprop="price">'.AuctionShortcodes::format_price( $realized ). '</span></li>';
+?>
 		<li><strong>More Information:</strong><br />
         For more information on this or any other item, email
         us at: <a href="mailto:info@caseantiques.com?subject=Case%20Antiques%20Inquiry:%20<?php echo esc_attr( str_replace( ' ', '%20', get_the_title( $post->ID ) ) ) ?>">info@caseantiques.com</a></li>
@@ -139,24 +139,24 @@ function centric_item_auctioninfo(){
 /**
  * Modify the Genesis breadcrumbs
  *
- * @see wp_get_post_terms()	retrieves this item's `auction`.
- * @see wp_get_referer()	we use this to build a back link to the specific auction page that referered us here.
+ * @see wp_get_post_terms() retrieves this item's `auction`.
+ * @see wp_get_referer() we use this to build a back link to the specific auction page that referered us here.
  * @global object $post WordPress post object.
  *
  * @since 1.0.0
  *
- * @param array $crumbs Array of breadcrumbs.
- * @param array $args Breadcrumb arguments.
+ * @param array   $crumbs Array of breadcrumbs.
+ * @param array   $args   Breadcrumb arguments.
  * @return array Breadcrumbs.
  */
-function centric_item_build_crumbs( $crumbs, $args ){
+function centric_item_build_crumbs( $crumbs, $args ) {
 	global $post;
 	$terms = wp_get_post_terms( $post->ID, 'auction' );
 
 	$paged = '';
 	$paged_text = '';
 	$url = parse_url( wp_get_referer() );
-	if( $url['path'] ){
+	if ( $url['path'] ) {
 		preg_match( '/page\/([0-9]+)/', $url['path'], $matches );
 		$page = ( $matches )? $matches[1] : null;
 		$paged = ( $page )? 'page/' . $page . '/' : '';
@@ -164,7 +164,7 @@ function centric_item_build_crumbs( $crumbs, $args ){
 		$paged_text.= ( $page )? $page : '1';
 	}
 
-	if( $terms ){
+	if ( $terms ) {
 		$auction = $terms[0];
 		$crumbs[1] = '<a href="' . get_term_link( $auction->term_id, 'auction' ) . $paged . '" title="Back to ' . esc_attr( $auction->name ) . '">' . $auction->name . $paged_text . '</a> ' . $args['sep'] . ' ' . $post->post_title;
 	}
@@ -178,14 +178,14 @@ function centric_item_build_crumbs( $crumbs, $args ){
  *
  * @return void
  */
-function centric_item_next_prev_links(){
+function centric_item_next_prev_links() {
 ?>
 <div class="item-nav-fixed">
 	<?php
 	$prev_post = get_adjacent_post( true, null, true, 'auction' );
-	if( $prev_post ){
-	$thumbnail = AuctionShortcodes::get_gallery_image( $prev_post->ID, true );
-	?>
+	if ( $prev_post ) {
+		$thumbnail = AuctionShortcodes::get_gallery_image( $prev_post->ID, true );
+?>
 	<a class="item-nav previous" href="<?php echo get_permalink( $prev_post->ID ); ?>" title="<?php _e( 'Previous post:', 'centric' ); echo ' ' . esc_attr( get_the_title( $prev_post->ID ) ) ?>">
 		<span class="arrow">&nbsp;</span>
 		<span class="preview" style="background-image: url(<?php echo $thumbnail; ?>)"><?php echo get_the_title( $prev_post->ID ) ?></span>
@@ -194,9 +194,9 @@ function centric_item_next_prev_links(){
 	<?php
 	}
 	$next_post = get_adjacent_post( true, null, false, 'auction' );
-	if( $next_post ){
-	$thumbnail = AuctionShortcodes::get_gallery_image( $next_post->ID, true );
-	?>
+	if ( $next_post ) {
+		$thumbnail = AuctionShortcodes::get_gallery_image( $next_post->ID, true );
+?>
 	<a class="item-nav next" href="<?php echo get_permalink( $next_post->ID ); ?>" title="<?php _e( 'Next post:', 'centric' ); echo ' ' . esc_attr( get_the_title( $next_post->ID ) ) ?>">
 		<span class="arrow">&nbsp;</span>
 		<span class="preview" style="background-image: url(<?php echo $thumbnail; ?>)"><?php echo get_the_title( $next_post->ID ) ?></span>
