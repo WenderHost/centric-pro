438f15d2f6ade8898c637fb9000777af3f7311d9
Michael Wender <michael@michaelwender.com>
2018-04-17 11:17:15 -0400

    Updating footer address

--- functions.php
+++ functions.php
@@ -37,7 +37,7 @@ function centric_load_scripts() {
 //* Footer Copyright
 add_filter('genesis_footer_creds_text', 'caseanti_footer_creds_filter');
 function caseanti_footer_creds_filter( $creds ) {
-	$creds = '[footer_copyright] ' . get_bloginfo( 'title' ) . '. All rights reserved.<br />Auctions in Association with:<br />Case Antiques, Inc. - TNGL#5157, TNGBL#5387 - 2240 Sutherland Avenue, Suite 101 - Knoxville, TN 37919 - (865) 558-3033';
+	$creds = '[footer_copyright] ' . get_bloginfo( 'title' ) . '. All rights reserved.<br />Auctions in Association with:<br />Case Antiques, Inc. - TNGL#5157, TNGBL#5387 - 4310 Papermill Drive, Knoxville, TN 37909 - (865) 558-3033';
 	return $creds;
 }
 
