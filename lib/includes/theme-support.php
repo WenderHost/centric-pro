c1fc9fea3b3ae9e712265bae697ae128cb2c11fb
Michael Wender <michael@michaelwender.com>
2018-10-23 10:11:50 -0400

    Custom callback for theme background-image

--- lib/includes/theme-support.php
+++ lib/includes/theme-support.php
@@ -7,7 +7,83 @@ add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list'
 add_theme_support( 'genesis-responsive-viewport' );
 
 //* Add support for custom background
-add_theme_support( 'custom-background' );
+function _centric_pro_custom_background_cb(){
+// $background is the saved custom image, or the default image.
+    $background = set_url_scheme( get_background_image() );
+
+    // $color is the saved custom color.
+    // A default has to be specified in style.css. It will not be printed here.
+    $color = get_background_color();
+
+    if ( $color === get_theme_support( 'custom-background', 'default-color' ) ) {
+        $color = false;
+    }
+
+    if ( ! $background && ! $color ) {
+        if ( is_customize_preview() ) {
+            echo '<style type="text/css" id="custom-background-css"></style>';
+        }
+        return;
+    }
+
+    $style = $color ? "background-color: #$color;" : '';
+
+    if ( $background ) {
+        $image = ' background-image: url("' . esc_url_raw( $background ) . '");';
+
+        // Background Position.
+        $position_x = get_theme_mod( 'background_position_x', get_theme_support( 'custom-background', 'default-position-x' ) );
+        $position_y = get_theme_mod( 'background_position_y', get_theme_support( 'custom-background', 'default-position-y' ) );
+
+        if ( ! in_array( $position_x, array( 'left', 'center', 'right' ), true ) ) {
+            $position_x = 'left';
+        }
+
+        if ( ! in_array( $position_y, array( 'top', 'center', 'bottom' ), true ) ) {
+            $position_y = 'top';
+        }
+
+        $position = " background-position: $position_x $position_y;";
+
+        // Background Size.
+        $size = get_theme_mod( 'background_size', get_theme_support( 'custom-background', 'default-size' ) );
+
+        if ( ! in_array( $size, array( 'auto', 'contain', 'cover' ), true ) ) {
+            $size = 'auto';
+        }
+
+        $size = " background-size: $size;";
+
+        // Background Repeat.
+        $repeat = get_theme_mod( 'background_repeat', get_theme_support( 'custom-background', 'default-repeat' ) );
+
+        if ( ! in_array( $repeat, array( 'repeat-x', 'repeat-y', 'repeat', 'no-repeat' ), true ) ) {
+            $repeat = 'repeat';
+        }
+
+        $repeat = " background-repeat: $repeat;";
+
+        // Background Scroll.
+        $attachment = get_theme_mod( 'background_attachment', get_theme_support( 'custom-background', 'default-attachment' ) );
+
+        if ( 'fixed' !== $attachment ) {
+            $attachment = 'scroll';
+        }
+
+        $attachment = " background-attachment: $attachment;";
+
+        $style .= $image . $position . $size . $repeat . $attachment;
+    }
+?>
+<style type="text/css" id="custom-background-css">
+body.custom-background .home-featured { <?php echo trim( $style ); ?> }
+</style>
+<?php
+}
+$custom_background_args = [
+  'wp-head-callback' => '_centric_pro_custom_background_cb'
+];
+add_theme_support( 'custom-background', $custom_background_args );
 
 //* Add support for custom header
 add_theme_support( 'custom-header', array(
